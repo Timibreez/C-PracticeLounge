30aa4fb9df3257abc9a231f07cf9b5ee015c5a9b
Timibreez <ffftimi@gmail.com>
2022-09-19 08:44:49 +0100

    while loops

---
+++ PracticeLounge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLounge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            while (index <=5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
