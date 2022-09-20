75f329f8f96b7cd976385f251c0b73537d20a297
Timibreez <ffftimi@gmail.com>
2022-09-20 08:50:23 +0100

    arrays

--- PracticeLounge/Program.cs
+++ PracticeLounge/Program.cs
@@ -10,13 +10,41 @@ namespace PracticeLounge
     {
         static void Main(string[] args)
         {
-            int index = 1;
+            //string secretWord = "timibreez";
+            //string guessedWord = "";
+            //bool outOfGuesses = false;
+            //int guessCount = 0;
+            //int guessLimit = 3;
 
-            while (index <=5)
+            //while (guessedWord != secretWord && !outOfGuesses)
+            //{
+            //    if(guessCount < guessLimit)
+            //    {
+            //        Console.WriteLine("Enter a word");
+            //        guessedWord = Console.ReadLine();
+            //        guessCount++;
+            //    }
+            //    else
+            //    {
+            //        outOfGuesses = true;
+            //    }
+            //}
+            //if (outOfGuesses)
+            //{
+            //    Console.WriteLine("You Lose");
+            //}else
+            //{
+            //    Console.WriteLine("You Win");
+            //}
+
+            int[,] numGrid =
             {
-                Console.WriteLine(index);
-                index++;
-            }
+                { 1, 2 },
+                {3,  4 },
+                {6, 5 },
+            };
+
+            Console.WriteLine(numGrid[1, 2]);
 
             Console.ReadLine();
         }
